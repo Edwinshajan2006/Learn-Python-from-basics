ef83c6983b31b7c6e3b5d456adaf1266b7bdc81f
Edwin <edwinshajan2006@gmail.com>
2026-07-18 19:57:03 +0530

    done a few basic elements of python code would do more

---
+++ simpleforloop.py
@@ -0,0 +1,9 @@
+age =19
+ifhappy= True
+
+if ifhappy==True:
+    print("You are the happy")
+elif ishappy==False:
+    print("you are no longer happy")
+else:
+    print("you are just numb")
---
+++ simpleifloop.py
@@ -0,0 +1,7 @@
+age =29
+if age>19:
+
+    print("You are no longer underage") 
+
+else:
+    print("You are underage..")
