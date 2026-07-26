d0774543d9e101000c34f95a050ae1967449495a
Edwin <edwinshajan2006@gmail.com>
2026-07-26 10:21:00 +0530

    Describe today's work

---
+++ oddoreven.py
@@ -0,0 +1,8 @@
+def iswhat(a):
+    if a/2 :
+            print("The entered number is Even")
+    else:
+          print("The entered number is Odd")
+
+a=float(input ( "Enter the number you wanna check \n"  ))
+iswhat(a)
---
+++ sqrt.py
@@ -0,0 +1,7 @@
+import math
+
+def sqrt(a):
+    print(math.isqrt(a))
+
+a=int(input("Enter the number you wanna find square root \n "))
+sqrt(a)
